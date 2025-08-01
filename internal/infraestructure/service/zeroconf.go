7b8be47e8b96fc7119b8fd6574a5d2701616d22e
Ajax Lima de Matos <ajaxlima94@gmail.com>
2025-08-01 21:49:08 +0000

    fix: update service configuration and implement device connection logic

--- internal/infraestructure/service/zeroconf.go
+++ internal/infraestructure/service/zeroconf.go
@@ -1,10 +1,13 @@
 package service
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"os"
 	"os/signal"
 	"synk/internal/domain"
+	"time"
 
 	"synk/pkg/logger"
 	"syscall"
@@ -48,7 +51,7 @@ func (z *ZeroconfService) Start() error {
 		service.Stop()
 		os.Exit(0)
 	}()
-
+	z.findAndConnectToNode("471d424ac743c8fd")
 	return nil
 }
 
@@ -60,4 +63,52 @@ func (z *ZeroconfService) Stop() error {
 		return nil
 	}
 	return nil
+}
+
+func (z *ZeroconfService) findAndConnectToNode(targetID string) {
+	fmt.Printf("Attempting to add device with ID: %s\n", targetID)
+	timeout := 10 * time.Second
+	resolver, err := zeroconf.NewResolver(nil)
+	if err != nil {
+		log.Error(fmt.Sprintf("Failed to initialize resolver: %s", err))
+	}
+
+	entries := make(chan *zeroconf.ServiceEntry)
+	go func() {
+		for entry := range entries {
+			fmt.Printf("Dispositivo encontrado: %v\n", entry)
+			fmt.Printf("  Nome: %s\n", entry.Instance)
+			fmt.Printf("  Endereço: %v\n", entry.AddrIPv4)
+			fmt.Printf("  Porta: %d\n", entry.Port)
+			fmt.Printf("  Metadados: %v\n", entry.Text)
+			if entry.Instance == fmt.Sprintf("Synk-%s", targetID) {
+				fmt.Printf("Conectando ao dispositivo com ID: %s\n", targetID)
+				connectToDevice(entry)
+				return
+			}
+		}
+	}()
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	err = resolver.Browse(ctx, z.config.Service.Type, z.config.Service.Domain, entries)
+	if err != nil {
+		log.Error(fmt.Sprintf("Failed to browse for services: %s", err))
+	}
+
+	<-ctx.Done()
+	fmt.Println("Busca de serviços concluída.")
+}
+
+func connectToDevice(entry *zeroconf.ServiceEntry) {
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", entry.AddrIPv4[0], entry.Port), 5*time.Second)
+	if err != nil {
+		fmt.Println("Failed to connect to device:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to the device! Ready to exchange keys and synchronize.")
+	// Aqui você implementaria a lógica de troca de chaves e sincronização
+	// usando a chave pública do outro dispositivo.
 }
